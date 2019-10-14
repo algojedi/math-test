9becdfa60ad8b504936388890c015303d7447a17
algojedi <vicbanwait@hotmail.com>
2019-10-14 10:47:01 -0400

    fixed issues

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,14 +3,12 @@ import { Link } from 'react-router-dom';
 
 import { auth } from '../../firebase/firebase.utils';
 
-//import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
   <div className='header'>
-    <Link className='logo-container' to='/'>
-      {/* <Logo className='logo' /> */}
+    <Link className='add-here' to='/'>
       Math Test
     </Link>
     <div className='options'>
