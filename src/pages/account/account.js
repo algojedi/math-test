303abc902147e71a051d43008eeaa18fa637ede1
algojedi <vicbanwait@hotmail.com>
2019-10-26 17:23:25 -0400

    added account pg

---
+++ src/pages/account/account.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Account = ({name, operator, percentage}) => {
+    return ( 
+        <div>
+            <h1>Account Details</h1>
+            <h2>{name}</h2>
+            <table>
+                <row>
+                    <th>
+                        Date
+                    </th>
+                    <th>
+                        Operator
+                    </th>
+                    <th>
+                        Percent
+                    </th>
+                </row>
+            </table>
+        </div>
+     );
+}
+ 
+export default Account;
